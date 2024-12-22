f6733463cf81c8e997707a35722f671d5a469913
rwiley <30681834+Cerkoryn@users.noreply.github.com>
2024-12-22 17:06:40 -0500

    Add logic for drep_always_abstain and drep_always_no_confidence

--- api/update_dreps.py
+++ api/update_dreps.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from http.server import BaseHTTPRequestHandler
 import requests
 import json
@@ -22,17 +23,38 @@ def fetch_drep_list():
     try:
         response = requests.get(drep_list_url, timeout=3)
         response.raise_for_status()
-    except (requests.exceptions.RequestException, requests.exceptions.Timeout):
+    except (requests.exceptions.RequestException, requests.exceptions.Timeout) as e:
         print({'error': 'Failed to fetch dRep list.'})
+        print(e)
         return None
     if response is None:
         print({'error': 'Response is empty.'})
+        print(e)
         return None
     
     drep_list_data = response.json()
-    return [drep['drep_id'] for drep in drep_list_data if 'drep_id' in drep]
+    drep_ids = [drep['drep_id'] for drep in drep_list_data if 'drep_id' in drep]
+    drep_ids.append("drep_always_abstain")
+    drep_ids.append("drep_always_no_confidence")
+    return drep_ids
 
 class handler(BaseHTTPRequestHandler):
+
+    def authenticate_request(self):
+            cron_token = os.getenv("CRON_SECRET")
+            auth_header = self.headers.get('Authorization')
+
+            if not auth_header or not auth_header.startswith('Bearer '):
+                self.send_json_response(403, {'error': 'Forbidden'})
+                return False
+
+            request_token = auth_header.split(' ')[1]
+
+            if request_token != cron_token:
+                self.send_json_response(403, {'error': 'Forbidden'})
+                return False
+
+            return True
 
     def send_json_response(self, status_code, body):
         self.send_response(status_code)
@@ -41,17 +63,7 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(body).encode('utf-8'))
 
     def do_GET(self):
-        cron_token = os.getenv("CRON_SECRET")
-        auth_header = self.headers.get('Authorization')
-
-        if not auth_header or not auth_header.startswith('Bearer '):
-            self.send_json_response(403, {'error': 'Forbidden'})
-            return
-
-        request_token = auth_header.split(' ')[1]
-
-        if request_token != cron_token:
-            self.send_json_response(403, {'error': 'Forbidden'})
+        if not self.authenticate_request():
             return
 
         start_time = time.time()
@@ -66,7 +78,6 @@ class handler(BaseHTTPRequestHandler):
             "Content-Type": "application/json"
         }
 
-
         dreps = []
         for i in range(0, len(drep_list), 50):
             batch_drep_ids = drep_list[i:i+50]
@@ -76,44 +87,54 @@ class handler(BaseHTTPRequestHandler):
             try:
                 response = requests.post(drep_info_url, headers=headers, json=payload, timeout=5)
                 response.raise_for_status()
-            except (requests.exceptions.RequestException, requests.exceptions.Timeout):
+            except (requests.exceptions.RequestException, requests.exceptions.Timeout) as e:
                 self.send_json_response(500, {'error': 'Failed to fetch dRep info.'})
+                print(e)
                 return
 
             data = response.json()
             if not isinstance(data, list):
                 self.send_json_response(500, {'error': 'Unexpected response format.'})
+                print(e)
                 return
             if data:
                 dreps.extend(data)
             else:
                 self.send_json_response(500, {'error': 'Response is empty.'})
+                print(e)
                 return
         
         final_data = []
-        for dRep in dreps:
+        # Defining here to use with threads.
+        def process_drep(dRep):
             given_name = None
             active_power = int(dRep.get('amount', 0))
-            if active_power is None or active_power <= 0:
-                continue
-            
+            if active_power <= 0:
+                return None
+
             meta_url = dRep.get('meta_url')
-            # Lazy way to not have to do a request to everyone's URL.
-            # Will probably miss some who host on other places.
             if meta_url and ("ipfs" in meta_url or "github" in meta_url):
                 try:
-                    response = requests.get(meta_url, timeout=2)
+                    response = requests.get(meta_url, timeout=1)
                     response.raise_for_status()
                     given_name = is_valid_jsonld(response)
                 except (requests.exceptions.RequestException, requests.exceptions.Timeout):
                     given_name = None
-            
-            final_data.append({
+
+            return {
                 'drep_id': dRep.get('drep_id'),
                 'is_active': dRep.get('active'),
                 'active_power': active_power,
                 'given_name': given_name
-            })
+            }
+
+        # Use ThreadPoolExecutor to process concurrently
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            futures = {executor.submit(process_drep, dRep): dRep for dRep in dreps}
+            for future in as_completed(futures):
+                result = future.result()
+                if result:
+                    final_data.append(result)
 
         final_data.sort(key=lambda x: x['active_power'], reverse=True)
         
